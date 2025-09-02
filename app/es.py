706ed5616d18a3edea0e7605f66a6648e43b4642
browngreg <gregory.yazidi@gmail.com>
2025-09-03 00:48:48 +0200

    Initial commit: ELK + FastAPI fake-news demo

--- app/es.py
+++ app/es.py
@@ -1,7 +1,10 @@
+
 from elasticsearch import Elasticsearch
 import os
+from dotenv import load_dotenv
 
 def get_es() -> Elasticsearch:
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
     host = os.getenv('ES_HOST', 'http://localhost:9200')
     user = os.getenv('ES_USER', 'elastic')
     password = os.getenv('ES_PASSWORD', 'changeme')
